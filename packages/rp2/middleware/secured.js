2aadae08d2cd74e30d24ca21d62ee790f1a29ca5
robertmozeika <robertmozeika20@gmail.com>
2019-09-03 14:42:18 -0400

    secur middleware

--- packages/rp2/middleware/secured.js
+++ packages/rp2/middleware/secured.js
@@ -2,6 +2,6 @@ module.exports = function () {
     return function secured (req, res, next) {
       if (req.user) { return next(); }
       req.session.returnTo = req.originalUrl;
-      res.redirect('/login');
+      res.redirect('/auth/login');
     };
   };

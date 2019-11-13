ed47ea0b57da8fe9da944878b852015260f789e9
rmozeika <robertmozeika@gmail.com>
2019-11-13 18:55:22 -0500

    default

--- .bin/monorift.sh
+++ .bin/monorift.sh
@@ -1,14 +1,14 @@
-CMD="cm"
+CMD=$1
 
-# function provision() {
-#     cd ../packages/devops/1a/opts && ./provision.sh
-# }
+function provision() {
+    cd ../packages/devops/1a/opts && ./provision.sh
+}
 
 
-# if [ $CMD = "cluster start" ]; then
-#     echo "TRUE"
-#     provision()
-# fi
+if [ $CMD = "cluster start" ]; then
+    echo "TRUE"
+    provision
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ $CMD = "cm" ]; then

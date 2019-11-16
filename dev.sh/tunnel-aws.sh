5f05485519a556fa279613b8fe8f2713e4ef8ce7
rmozeika <robertmozeika@gmail.com>
2019-11-16 15:47:13 -0500

    default

--- dev.sh/tunnel-aws.sh
+++ dev.sh/tunnel-aws.sh
@@ -1,1 +1,8 @@
+# node server
 ssh -N -L 8080:localhost:8080 ec2-3-17-13-33.us-east-2.compute.amazonaws.com
+
+#nginx
+ssh -N -L 80:localhost:80 ec2-3-17-13-33.us-east-2.compute.amazonaws.com
+
+#mongo 
+ssh -N -L 27017:localhost:27017 ec2-3-17-13-33.us-east-2.compute.amazonaws.com

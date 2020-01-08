87019bd1b1518ae923444a476209b4132798aa78
rmozeika <robertmozeika@gmail.com>
2020-01-08 14:44:07 -0500

    default

--- dev.sh/backup-cloud.sh
+++ dev.sh/backup-cloud.sh
@@ -1,12 +1,12 @@
-rsync --recursive -v -e ssh bobby@awsbob:/home/bobby ./backup/.
-cp ./backup/bobby/privkey.pem ./private/privkey.pem
-cp ./backup/bobby/fullchain.pem ./private/fullchain.pem
-cp -R ./backup/bobby/letsencrypt ./packages/devops/data/etc/
-cp -R ./backup/bobby/nginx ./packages/devops/data/etc/
-cp ./backup/bobby/privkey.pem ./packages/devops/data/keys/privkey.pem
-cp ./backup/bobby/fullchain.pem ./packages/devops/data/keys/fullchain.pem
+rsync --recursive -v -e ssh --exclude 'monorift/*' ec2-user@aws:/home/ec2-user ./private/ec2-files/.
+# cp ./backup/bobby/privkey.pem ./private/privkey.pem
+# cp ./backup/bobby/fullchain.pem ./private/fullchain.pem
+# cp -R ./backup/bobby/letsencrypt ./packages/devops/data/etc/
+# cp -R ./backup/bobby/nginx ./packages/devops/data/etc/
+# cp ./backup/bobby/privkey.pem ./packages/devops/data/keys/privkey.pem
+# cp ./backup/bobby/fullchain.pem ./packages/devops/data/keys/fullchain.pem
 
-rsync --recursive -v -e ssh ec2-user@aws:/home/ec2-user ./backup/.
+# rsync --recursive -v -e ssh ec2-user@aws:/home/ec2-user ./backup/.
 
 # rsync --recursive -v -e ssh bobby@awsbob:/home/bobby ./backup/.
 

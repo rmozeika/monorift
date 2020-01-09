9679c9e76ca944fb6b4bf830f024de650cb0aaa4
rmozeika <robertmozeika@gmail.com>
2020-01-08 20:19:53 -0500

    default

--- packages/deploy/hooks.js
+++ packages/deploy/hooks.js
@@ -8,6 +8,7 @@ const { exec } = require('child_process');
 const fs = require('fs').promises;
 const utils = require('./utils');
 const qs = require('querystring');
+const crypto = require('crypto');
 
 console.log(utils);
 var currentPath = process.cwd();
@@ -32,22 +33,33 @@ proxy.on('connect', (req, cltSocket, head) => {
 		cltSocket.pipe(srvSocket);
 	});
 });
+const hmac = crypto.createHmac('sha1', secret);
 
 // Now that proxy is running
 proxy.listen(9090, '127.0.0.1', () => {
 	console.log('listening');
 	proxy.on('request', (req, res) => {
+		crypto.verify(null);
+		const altshakey = 'sha1=86274eaac703a16fab76ce308e6968a64ef19079';
+		const shakey = req.headers['X-Hub-Signature'];
+		const verify = crypto.createVerify('SHA1');
+
 		console.log(req);
 		const writePath = path.join(__dirname, 'history');
 		let rawData = '';
 		console.log('test');
 		req.on('data', (data, var2) => {
 			console.log(data, var2);
+
 			rawData += data;
 			const stringData = data.toString();
 			const parsedData = qs.parse(stringData);
 			console.log(parsedData);
-			const parsed = JSON.parse(parsedData.payload);
+			const payload = JSON.parse(parsedData.payload);
+			// console.log(verify.verify(publicKey, signature));
+			const digest = 'sha1=' + hmac.update(payload).digest('hex');
+			const checksum = req.get(sigHeaderName);
+			//crypto.verify(null, parsed, req.headers )
 			utils.writeFile(writePath, parsed); //comment1test
 		});
 		req.on('end', data => {

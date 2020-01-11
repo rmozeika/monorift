76480c05ac39eb43bf3e2fc507e3566fb221daa0
rmozeika <robertmozeika@gmail.com>
2020-01-11 13:40:16 -0500

    transfer files commands

--- dev.sh/transfer-files/index.js
+++ dev.sh/transfer-files/index.js
@@ -1,11 +1,15 @@
+const hardCodedCommand = 'dist'; // null
+
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
+const exec = util.promisify(require('child_process').exec);
+
 const path = require('path');
 
 const currentPath = process.cwd();
 const packagesPath = path.resolve(currentPath, 'packages');
 const deployPath = path.resolve(packagesPath, 'deploy');
-const scriptToRun = process.argv[2];
+const scriptToRun = hardCodedCommand || process.argv[2];
 
 const transferDeployConf = async () => {
 	const sendConf = path.resolve(deployPath, '.bin', 'update-conf.sh');
@@ -18,12 +22,22 @@ const trasnferDeployService = async () => {
 	console.log(stdout, stderr);
 };
 
+const transferDistWeb = async () => {
+	const cmd = `rsync --recursive -v -e ssh  ./dist.web/. awsmono:/home/monorift/monorift/dist.web/.`;
+
+	const sendingDist = await exec(cmd, { cwd: currentPath });
+	console.log(sendingDist.stdout, sendingDist.stderr);
+};
+
 switch (scriptToRun) {
 	case 'deployconf':
 		transferDeployConf();
 		break;
 	case 'service':
 		trasnferDeployService();
+		break;
+	case 'dist':
+		transferDistWeb();
 		break;
 	default:
 		console.log('No known statements, sending all');

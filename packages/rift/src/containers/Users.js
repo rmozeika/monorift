52b47c0d9f14112b3006ec553f04b5b69f3a667b
rmozeika <robertmozeika@gmail.com>
2020-02-23 14:23:02 -0500

    fix styling + before programatic action btns

--- packages/rift/src/containers/Users.js
+++ packages/rift/src/containers/Users.js
@@ -66,7 +66,13 @@ class Users extends React.Component {
 		this.props.setViewTab(1);
 	}
 	render() {
-		const { gotOnlineUsers, online, themedStyle, customHeights } = this.props;
+		const {
+			gotOnlineUsers,
+			online,
+			themedStyle,
+			customHeights,
+			mobile
+		} = this.props;
 		const loading = (
 			<Layout style={styles.row}>
 				<Text>Loading...</Text>
@@ -97,16 +103,18 @@ class Users extends React.Component {
 							To Call
 						</Button>
 					</Layout> */}
-					<ToCallButton
-						style={[
-							styles.buttonRow,
-							themedStyle.buttonRow,
-							{ height: customHeights.callButton }
-						]}
-						onPress={this.goTalk.bind(this)}
-						buttonHeight={customHeights.callButton}
-						themedStyle={themedStyle.buttonRow}
-					/>
+					{mobile && (
+						<ToCallButton
+							style={[
+								styles.buttonRow,
+								themedStyle.buttonRow,
+								{ height: customHeights.callButton }
+							]}
+							onPress={this.goTalk.bind(this)}
+							buttonHeight={customHeights.callButton}
+							themedStyle={themedStyle.buttonRow}
+						/>
+					)}
 				</Layout>
 			);
 		};
@@ -126,12 +134,14 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 	};
 };
 const mapStateToProps = (state, ownProps) => {
-	const { users } = state;
+	const { users, view } = state;
 	const { online } = users;
 	const { gotOnlineUsers } = online;
+	const { mobile } = view;
 	return {
 		online: online.users,
-		gotOnlineUsers
+		gotOnlineUsers,
+		mobile
 	};
 };
 export default connect(mapStateToProps, mapDispatchToProps)(UsersStyled);

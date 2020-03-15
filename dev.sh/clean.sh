b3ffa907cfde5ec85e3cef309bdf3c59aae38525
rmozeika <robertmozeika@gmail.com>
2020-03-15 19:57:18 -0400

    re-add lock

--- dev.sh/clean.sh
+++ dev.sh/clean.sh
@@ -1,5 +1,16 @@
 #/bin/bash
 rm -rf ./node_modules
-rm -rf ./packages/rift/node_modules
-rm -rf ./packages/rp2/node_modules
 rm yarn.lock
+#rm package-lock.json
+
+rm -rf ./packages/rift/node_modules
+#rm -f ./packages/rift/package-lock.json
+rm -f ./packages/rift/yarn.lock
+
+rm -rf ./packages/rp2/node_modules
+#rm -f ./packages/rp2/package-lock.json
+#rm -f ./packages/rp2/yarn.lock
+
+rm -rf ./packages/code-parser/node_modules
+#rm -f ./packages/code-parser/package-lock.json
+#rm -f ./packages/code-parser/yarn.lock

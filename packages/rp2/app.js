8289ff2ad2e851c2293e8fbdcb0cf423326dfffb
rmozeika <43491454+rmozeika@users.noreply.github.com>
2020-06-03 01:07:09 -0400

    graphql client side - 0.6.1 (#49)
    * version 0.6.1
    
    * correct new packages docker

--- packages/rp2/app.js
+++ packages/rp2/app.js
@@ -21,7 +21,7 @@ const CallSocket = require('./socket/call');
 const UsersSocket = require('./socket/users');
 const graphqlHTTP = require('express-graphql');
 const Schema = require('./data-service/data-model/users/graphql.schema.js');
-console.log('VERSION', '0.5.3');
+console.log('VERSION', '0.6.1');
 const { ApolloServer, gql } = require('apollo-server-express');
 
 var app = express();
